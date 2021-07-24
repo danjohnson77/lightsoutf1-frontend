b843b8d5971abb2ebb793a5aaca2474106c966bc
Daniel Johnson <danjohnson77@gmail.com>
2021-07-23 18:00:02 -0700

    fix env vars again

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = {
     SENDGRID_API_KEY:
       "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",
     NEXTAUTH_URL: "http://localhost:3000",
-    API_URL: "http://localhost:5000",
+    API_URL: "https://morning-peak-00042.herokuapp.com",
     MONGO_URI:
       "mongodb+srv://lof1db:FZmqm5yd3jz4aamk@lof1.lj2wl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    BASE_URL: "http://localhost:3000",
+    BASE_URL: "https://lightsoutf1-frontend.vercel.app",
   },
 };
